69f25db6a14f69f17b4764c595f74ffb9cbaad06
andre10dias <andre10dias@ymail.com>
2023-07-12 19:46:46 -0300

    Projeto livraria - crud de livros

--- src/app.js
+++ src/app.js
@@ -17,9 +17,38 @@ app.get('/livros', (req, res) => {
     res.status(200).json(livros);
 });
 
+app.get('/livros/:id', (req, res) => {
+    let index = buscaLivro(req.params.id);
+    res.json(livros[index]);
+});
+
 app.post('/livros', (req, res) => {
     livros.push(req.body);
     res.status(201).send('Livro cadastrado com sucesso!');
+}); 
+
+app.put('/livros/:id', (req, res) => {
+    let index = buscaLivro(req.params.id);
+    livros[index].titulo = req.body.titulo;
+    res.json(livros);
 });
+
+app.put('/livros/:id', (req, res) => {
+    let index = buscaLivro(req.params.id);
+    livros[index].titulo = req.body.titulo;
+    res.json(livros);
+});
+
+app.delete('/livros/:id', (req, res) => {
+    let {id} = req.params;
+    let index = buscaLivro(id);
+    let titulo = livros[index].titulo;
+    livros.splice(index, 1);
+    res.send(`Livro "${titulo}" removido com sucesso`);
+});
+
+function buscaLivro (id) {
+    return livros.findIndex(livro => livro.id == id);
+}
 
 export default app;
